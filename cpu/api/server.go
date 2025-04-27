51c5081b14efcada08decd3d50568fae9196d931
Valentina <70960916+ValentinaArb@users.noreply.github.com>
2025-04-27 12:50:18 -0300

    cpu de nuevo server

---
+++ cpu/api/server.go
@@ -0,0 +1,19 @@
+package api
+import (
+	"github.com/sisoputnfrba/tp-golang/cpu/api/handlers"
+	"github.com/sisoputnfrba/tp-golang/utils/server"
+	"github.com/sisoputnfrba/tp-golang/cpu/global"
+	"net/http"
+	"fmt"
+)
+
+func CrearServer() *server.Server {
+	configServer := server.Config{
+		Port: global.CpuConfig.Port_Cpu,
+		Handlers: map[string]http.HandlerFunc{
+			"POST /responder": handlers.RecibirPaquete,
+		},
+	}
+	fmt.Printf("🟢 CPU prendido en http://localhost:%d\n", global.CpuConfig.Port_Cpu)
+	return server.NuevoServer(configServer)
+}
--- cpu/cpu.go
+++ cpu/cpu.go
@@ -2,15 +2,17 @@ package main
 
 import (
 	"os"
-/* 	"github.com/sisoputnfrba/tp-golang/cpu/api" */
+	"github.com/sisoputnfrba/tp-golang/cpu/api"
+	"github.com/sisoputnfrba/tp-golang/cpu/api/handlers"
 	"github.com/sisoputnfrba/tp-golang/cpu/global"
-	utilsCpu "github.com/sisoputnfrba/tp-golang/cpu/utilsCpu"
-/* 	"github.com/sisoputnfrba/tp-golang/utils/logger" */
-/* 	"github.com/sisoputnfrba/tp-golang/utils/paquetes" */
+	"github.com/sisoputnfrba/tp-golang/utils/logger"
+	/* "github.com/sisoputnfrba/tp-golang/utils/paquetes" */
 	"fmt"
 )
 
 func main() {
+	// configurar logger e inicializar config
+
 	if len(os.Args) < 2 {
 		fmt.Println("Uso: ./cpu <ID_CPU>")
 		return
@@ -19,10 +21,10 @@ func main() {
 	idCPU := os.Args[1]
 	global.InitGlobal(idCPU)
 
-	utilsCpu.RealizarHandshakeConKernel()
+	handlers.RealizarHandshakeConKernel()
 	
 	defer global.LoggerCpu.CloseLogger()
-/* 	s := api.CrearServer() 
+	s := api.CrearServer()
 	
 	go func() {
 		err_server := s.Iniciar()
@@ -30,10 +32,10 @@ func main() {
 			global.LoggerCpu.Log("Error al iniciar el servidor: "+err_server.Error(), log.ERROR)
 			os.Exit(1)
 		}
-	}()
-*/
-/* 	paqueteNuevo := paquetes.LeerConsola()	
-	paquetes.GenerarYEnviarPaquete(paqueteNuevo, "127.0.0.1") */
+		}()
 
+	/* paqueteNuevo := paquetes.LeerConsola()	
+	paquetes.GenerarYEnviarPaquete(paqueteNuevo, "127.0.0.1")
+ */
 	select{}
 }
