6ce96b37a8ce325ddd8af507d9894a82de54642d
delfisartorio <msartorio@frba.utn.edu.ar>
2025-06-18 13:59:37 -0300

    Descomentarios io

--- io/io.go
+++ io/io.go
@@ -17,10 +17,8 @@ func main() {
 	global.InitGlobal()
 	defer global.LoggerIo.CloseLogger()
 
-	//paso 1: leer como parametro el nombre de la interaz io desde consola
-	//para chequear que se pase el nombre
 	if len(os.Args) < 2 {
-		global.LoggerIo.Log("Tenés que pasar el nombre de una interfaz. Ejemplo: go run ./src/io.go teclado", log.ERROR) //Ver esto, porque no ejecuta así.. si no tiramos un cd directo
+		global.LoggerIo.Log("Ejemplo: go run ./src/io.go teclado", log.ERROR) 
 		return
 	}
 s := api.CrearServer()
@@ -40,7 +38,7 @@ s := api.CrearServer()
 		PuertoIO: global.IoConfig.Port_Io,
 	}
 
-	err := utilsIO.HandshakeConKernel(infoIO) //Usa el archivo utilsIo
+	err := utilsIO.HandshakeConKernel(infoIO) 
 	if err != nil {
 		fmt.Println("Error al enviar paquete al Kernel:", err)
 		return
